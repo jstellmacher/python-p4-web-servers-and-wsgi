7c7f723dae5acc5d0c7673321574fbecc260732a
Jai <jaijai.stell@gmail.com>
2023-05-23 15:41:18 -0700

    done

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+#jai's code
+from werkzeug.wrappers import Request, Response
+import werkzeug
+
+run_simple = werkzeug.run_simple
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+if __name__ == '__main__':
+   
+    run_simple(
+        hostname = 'localhost',
+        port=5555,
+        application = application
+    )
+    # port = 5555
+
+
